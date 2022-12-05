d0c0d41c73f551cd8aae17c01e9dc2270c9436f4
dymafr <32801166+dymafr@users.noreply.github.com>
2022-12-05 13:25:19 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,44 +1,45 @@
-import { Component, OnInit } from "@angular/core";
-import { MatDialog } from "@angular/material/dialog";
-import { MatSnackBar } from "@angular/material/snack-bar";
-import { DialogComponent } from "./shared/components/dialog.component";
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { DialogComponent } from './shared/components/dialog.component';
+import { MatButtonToggleChange } from '@angular/material/button-toggle';
 
 @Component({
-  selector: "my-app",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"]
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  public chips: string[] = ["chip 1", "chip 2", "chip 3"];
+  public chips: string[] = ['chip 1', 'chip 2', 'chip 3'];
 
   constructor(private dialog: MatDialog, private snack: MatSnackBar) {}
 
-  toggle(event) {
-    console.log(event);
+  toggle(event: MatButtonToggleChange) {
+    console.log(event.value);
   }
 
-  removeChip(event, chip: string) {
-    this.chips = this.chips.filter(el => el !== chip);
+  removeChip(chip: string) {
+    this.chips = this.chips.filter((el) => el !== chip);
   }
 
   openDialog() {
     const ref = this.dialog.open(DialogComponent, {
-      width: "800px",
-      height: "400px",
-      data: "Des données"
+      width: '800px',
+      height: '400px',
+      data: 'Des données',
     });
 
-    ref.afterClosed().subscribe(data => console.log(data));
+    ref.afterClosed().subscribe((data) => console.log(data));
   }
 
   openSnack() {
-    const ref = this.snack.open("Sauvegarde effectuée", "annuler", {
-      duration: 2000
+    const ref = this.snack.open('Sauvegarde effectuée', 'annuler', {
+      duration: 2000,
     });
 
-    ref.afterDismissed().subscribe(() => console.log("done"));
+    ref.afterDismissed().subscribe(() => console.log('done'));
 
-    ref.onAction().subscribe(() => console.log("action cliqué"));
+    ref.onAction().subscribe(() => console.log('action cliqué'));
   }
 
   ngOnInit() {}
